f04bb32011e3a1ba22d8163e7a2b69c4144bd91c
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2024-01-09 20:42:59 +0800

    Unreal Type Readers

---
+++ src/lib/UnrealTypeReaders.ts
@@ -0,0 +1,153 @@
+// Exports functions to read Unreal types from a SequentialReader.
+
+import SequentialReader from './SequentialReader';
+
+export function readFDateTime(reader: SequentialReader): Date {
+  // tick => 100 nanoseconds since 1/1/0001
+  const ticks = reader.readInt64();
+  const milliseconds = ticks / 10000n;
+  const epoch = Number(milliseconds - 62135596800000n);
+  return new Date(epoch);
+}
+
+export function readFString(reader: SequentialReader): string {
+  const length = reader.readInt();
+  if (length === 0) {
+    return '';
+  }
+
+  if (length > 0) {
+    // ASCII
+    const data = reader.slice(length);
+    const decoder = new TextDecoder('ascii');
+    return decoder.decode(data).slice(0, -1);
+  }
+
+  // UTF-16
+  const data = reader.slice(-length * 2);
+  const decoder = new TextDecoder('utf-16');
+  return decoder.decode(data).slice(0, -1);
+}
+
+export function readTArray<T>(reader: SequentialReader, readElement: (reader: SequentialReader) => T): T[] {
+  const length = reader.readInt();
+  const result = [];
+  for (let i = 0; i < length; i++) {
+    result.push(readElement(reader));
+  }
+  return result;
+}
+
+export function readTMap<K, V>(
+  reader: SequentialReader,
+  readKey: (reader: SequentialReader) => K,
+  readValue: (reader: SequentialReader) => V,
+): Map<K, V> {
+  const length = reader.readInt();
+  const result = new Map();
+  for (let i = 0; i < length; i++) {
+    const key = readKey(reader);
+    const value = readValue(reader);
+    result.set(key, value);
+  }
+  return result;
+}
+
+export interface ObjectReference {
+  level: string;
+  path: string;
+}
+
+export function readObjectReference(reader: SequentialReader): ObjectReference {
+  const level = readFString(reader);
+  const path = readFString(reader);
+  return { level, path };
+}
+
+export interface FGuid {
+  a: number;
+  b: number;
+  c: number;
+  d: number;
+}
+
+export function readFGuid(reader: SequentialReader) {
+  const a = reader.readUint();
+  const b = reader.readUint();
+  const c = reader.readUint();
+  const d = reader.readUint();
+  return { a, b, c, d };
+}
+
+export function readFMD5Hash(reader: SequentialReader) {
+  const bIsValid = reader.readInt() !== 0;
+  const hash = reader.slice(16);
+  return { bIsValid, hash };
+}
+
+export function readFText(reader: SequentialReader) {
+  // TODO: incomplete
+  const flags = reader.readUint(); // Unknown
+  const historyType = reader.readInt8(); // ETextHistoryType
+  if (historyType === -1) {
+    // ETextHistoryType::None
+    const hasCultureInvariantString = reader.readInt() !== 0;
+    if (hasCultureInvariantString) {
+      return readFString(reader);
+    }
+  }
+
+  throw new Error(`Not implemented: Unable to read FText with Flags=${flags.toString(2)}, HistoryType=${historyType}`);
+}
+
+interface FPropertyTag {
+  name: string; // Name/Key of property
+  type: string; // Type of property (Removed "Property" suffix)
+  size: number; // Property size (default is 0)
+  arrayIndex: number; // Index if an array (default is 0)
+  boolValue?: boolean; // a boolean property's value (default is false)
+  structName?: string; // Struct name if StructProperty.
+  enumName?: string; // Enum name if ByteProperty or EnumProperty
+  innerType?: string; // Inner type if ArrayProperty, SetProperty, or MapProperty (Remove "Property" suffix)
+  valueType?: string; // Value type if MapPropery
+  sizeOffset?: number; // location in stream of tag size member ?? (default is 0)
+  structGuid?: FGuid;
+  hasPropertyGuid: boolean; // (default is false)
+  propertyGuid?: FGuid;
+}
+
+export function readFPropertyTag(reader: SequentialReader) {
+  const name = readFString(reader);
+
+  if (name === 'None') {
+    return null;
+  }
+
+  const tag: Partial<FPropertyTag> = {
+    name,
+    type: readFString(reader).slice(0, -8), //Remove Property
+    size: reader.readInt(),
+    arrayIndex: reader.readInt(),
+  };
+
+  if (tag.type === 'Bool') {
+    tag.boolValue = reader.readByte() !== 0;
+  } else if (tag.type === 'Struct') {
+    tag.structName = readFString(reader);
+    tag.structGuid = readFGuid(reader);
+  } else if (tag.type === 'Enum' || tag.type === 'Byte') {
+    tag.enumName = readFString(reader);
+  } else if (tag.type === 'Array' || tag.type === 'Set' || tag.type === 'Map') {
+    tag.innerType = readFString(reader).slice(0, -8); //Remove Property
+    if (tag.type === 'Map') {
+      tag.valueType = readFString(reader).slice(0, -8); //Remove Property
+    }
+  }
+
+  tag.hasPropertyGuid = reader.readByte() !== 0;
+  if (tag.hasPropertyGuid) {
+    tag.propertyGuid = readFGuid(reader);
+  }
+
+  return tag as FPropertyTag;
+}
