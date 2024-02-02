3b11b18a0cd62e995238935b7640a8d5675fd7bf
PlutoyDev <plutoy.dev@outlook.com>
2024-02-02 15:12:59 +0800

    starting out on writer for save editing

---
+++ src/writer/SequentialWriter.ts
@@ -0,0 +1,109 @@
+export class SequentialWriter {
+  dataView: DataView;
+  offset: number;
+  autoIncrement: boolean;
+  littleEndian: boolean;
+  increaseAmt = 1024;
+
+  constructor(
+    buffer?: ArrayBuffer,
+    options: {
+      offset?: number;
+      autoIncrement?: boolean;
+      littleEndian?: boolean;
+    } = {},
+  ) {
+    this.dataView = new DataView(buffer ?? new ArrayBuffer(this.increaseAmt * 30));
+    this.offset = options?.offset ?? 0;
+    this.autoIncrement = options?.autoIncrement ?? false;
+    this.littleEndian = options?.littleEndian ?? true;
+  }
+
+  get buffer(): ArrayBuffer {
+    return this.dataView.buffer;
+  }
+
+  get length(): number {
+    return this.dataView.byteLength;
+  }
+
+  get remaining(): number {
+    return this.length - this.offset;
+  }
+
+  get isEOF(): boolean {
+    return this.remaining <= 0;
+  }
+
+  ensureSpace(size: number) {
+    if (this.remaining < size) {
+      const newBuffer = new ArrayBuffer(this.length + this.increaseAmt);
+      const newView = new Uint8Array(newBuffer);
+      newView.set(new Uint8Array(this.buffer));
+      this.dataView = new DataView(newBuffer);
+    }
+  }
+
+  writeInt8(value: number) {
+    this.ensureSpace(1);
+    this.dataView.setInt8(this.offset, value);
+    this.offset += 1;
+  }
+
+  writeByte(value: number) {
+    this.ensureSpace(1);
+    this.dataView.setUint8(this.offset, value);
+    this.offset += 1;
+  }
+
+  writeInt(value: number) {
+    this.ensureSpace(4);
+    this.dataView.setInt32(this.offset, value, this.littleEndian);
+    this.offset += 4;
+  }
+
+  writeUint(value: number) {
+    this.ensureSpace(4);
+    this.dataView.setUint32(this.offset, value, this.littleEndian);
+    this.offset += 4;
+  }
+
+  writeInt64(value: bigint | number) {
+    this.ensureSpace(8);
+    this.dataView.setBigInt64(this.offset, typeof value === 'number' ? BigInt(value) : value, this.littleEndian);
+    this.offset += 8;
+  }
+
+  writeUint64(value: bigint | number) {
+    this.ensureSpace(8);
+    this.dataView.setBigUint64(this.offset, typeof value === 'number' ? BigInt(value) : value, this.littleEndian);
+    this.offset += 8;
+  }
+
+  writeFloat(value: number) {
+    this.ensureSpace(4);
+    this.dataView.setFloat32(this.offset, value, this.littleEndian);
+    this.offset += 4;
+  }
+
+  writeDouble(value: number) {
+    this.ensureSpace(8);
+    this.dataView.setFloat64(this.offset, value, this.littleEndian);
+    this.offset += 8;
+  }
+
+  writeBytes(bytes: Uint8Array) {
+    const newSize = this.offset + bytes.length + this.increaseAmt;
+    const existing = new Uint8Array(this.buffer);
+    const newBuffer = new Uint8Array(new ArrayBuffer(newSize));
+    newBuffer.set(existing);
+    newBuffer.set(bytes, this.offset);
+    this.dataView = new DataView(newBuffer.buffer);
+  }
+
+  skip(length: number) {
+    // remains 0s to the buffer
+    this.ensureSpace(length);
+    this.offset += length;
+  }
+}
